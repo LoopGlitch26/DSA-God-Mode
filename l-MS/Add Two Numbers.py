707edb977a51878a661070fb9caaa57da1c2094f
Bravish Ghosh <grandson.baba.2012@gmail.com>
2021-06-24 16:34:33 +0530

    commit 1&2 leetcode

---
+++ l-MS/Add Two Numbers.py
@@ -0,0 +1,23 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not l1 or not l2:
+            return None
+        head = ListNode(0)
+        curr = head
+        carry = 0
+        while l1 or l2:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            curr.next = ListNode(carry % 10)
+            curr = curr.next
+            carry //= 10
+        if carry:
+            curr.next = ListNode(carry)
+        return head.next
